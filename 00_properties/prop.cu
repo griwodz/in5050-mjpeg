3d6f3499a2e361a93b182e37505c8a27c9e9a873
Carsten Griwodz <griff@ifi.uio.no>
2024-03-14 13:56:22 +0100

    small changes

--- 00_properties/prop.cu
+++ 00_properties/prop.cu
@@ -17,12 +17,26 @@ int main( )
         return -1;
     }
 
+    /* Many more properties are here:
+     * https://docs.nvidia.com/cuda/cuda-runtime-api/structcudaDeviceProp.html
+     */
+
+
     cout << "Device name: " << prop.name << endl;
     cout << "    CUDA Compute Capability: " << prop.major << "." << prop.minor << endl
          << "        Integrated GPU: " << (prop.integrated ? "yes" : "no") << endl
          << "    Memory:" << endl
          << "        Mapping host memory into the GPU: " << (prop.canMapHostMemory ? "yes" : "no") << endl
-         << "        Copy memory while a kernel runs:  " << (prop.deviceOverlap ? "yes" : "no") << endl
+         << "        Copy memory while a kernel runs : " << (prop.deviceOverlap ? "yes" : "no") << endl
+         << "                                          " << prop.deviceOverlap << endl
+         // << "        Can register host memory        : " << (prop.hostRegisterSupported ? "yes" : "no") << endl
+         // << "        Can register host memory (reda) : " << (prop.hostRegisterReadOnlySupported ? "yes" : "no") << endl
+         << "        Access registered mem with host pointer   : " << (prop.canUseHostPointerForRegisteredMem ? "yes" : "no") << endl
+         << "                                                    " << prop.canUseHostPointerForRegisteredMem << endl
+         << "        GPU can access mgmd mem coherent with CPU : " << (prop.concurrentManagedAccess ? "yes" : "no") << endl
+         << "                                                    " << prop.concurrentManagedAccess << endl
+         << "        CPU can access mgmd mem directly          : " << (prop.directManagedMemAccessFromHost ? "yes" : "no") << endl
+         << "                                                    " << prop.directManagedMemAccessFromHost << endl
          << "    Threads:" << endl
          << "        Number of threads in a warp : " << prop.warpSize << endl
          << "        Max threads in one block    : " << prop.maxThreadsPerBlock << endl
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 clean:
+	make -C 00_properties clean
 	make -C 01_cpu clean
-	make -C cuda-malloc clean
-	make -C cuda-managed clean
+	make -C 02_cuda_malloc clean
+	make -C 03_cuda_managed clean
 	make -C cuda-texture clean
-	make -C cudatex clean
-	make -C managed clean
+
