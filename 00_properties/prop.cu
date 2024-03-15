62bcfd40125f91bfdb059826e9804f7b1f278771
Carsten Griwodz <griff@ifi.uio.no>
2024-03-15 11:09:19 +0100

    add various unified memory related flags

--- 00_properties/prop.cu
+++ 00_properties/prop.cu
@@ -13,7 +13,8 @@ int main( )
     err = cudaGetDeviceProperties( &prop, dev );
     if( err != cudaSuccess )
     {
-        cerr << "Failed to read CUDA device properties from device " << dev << endl;
+        cerr << "Failed to read CUDA device properties from device " << dev << endl
+             << cudaGetErrorString(cudaGetLastError()) << endl;
         return -1;
     }
 
@@ -26,13 +27,24 @@ int main( )
     cout << "    CUDA Compute Capability: " << prop.major << "." << prop.minor << endl
          << "        Integrated GPU: " << (prop.integrated ? "yes" : "no") << endl
          << "    Memory:" << endl
-         << "        Mapping host memory into the GPU: " << (prop.canMapHostMemory ? "yes" : "no") << endl
-         << "        Copy memory while a kernel runs : " << (prop.deviceOverlap ? "yes" : "no") << endl
+         << "        Mapping host memory into the GPU          : " << (prop.canMapHostMemory ? "yes" : "no") << endl
+         << "        Copy memory while a kernel runs           : " << (prop.deviceOverlap ? "yes" : "no") << endl
          // << "        Can register host memory        : " << (prop.hostRegisterSupported ? "yes" : "no") << endl
          // << "        Can register host memory (reda) : " << (prop.hostRegisterReadOnlySupported ? "yes" : "no") << endl
          << "        Access registered mem with host pointer   : " << (prop.canUseHostPointerForRegisteredMem ? "yes" : "no") << endl
          << "        GPU can access mgmd mem coherent with CPU : " << (prop.concurrentManagedAccess ? "yes" : "no") << endl
          << "        CPU can access mgmd mem directly          : " << (prop.directManagedMemAccessFromHost ? "yes" : "no") << endl
+         << "        Unified memory support :" << endl
+         << "          Unified addressing   : " << prop.unifiedAddressing << endl
+         << "          All values must be 1 for full CUDA unified memory support" << endl
+         << "            prop.pageableMemoryAccess                   : " << prop.pageableMemoryAccess << endl
+         << "            prop.hostNativeAtomicSupported              : " << prop.hostNativeAtomicSupported << endl
+         << "            prop.pageableMemoryAccessUsesHostPageTables : " << prop.pageableMemoryAccessUsesHostPageTables << endl
+         << "            prop.directManagedMemAccessFromHost         : " << prop.directManagedMemAccessFromHost << endl
+         << "          Must be 1 for full CUDA managed memory support" << endl
+         << "            prop.pageableMemoryAccess : " << prop.pageableMemoryAccess << endl
+         << "          Must be 1 for unified addressing by no concurrent access" << endl
+         << "            prop.managedMemory : " << prop.managedMemory << endl
          << "    Threads:" << endl
          << "        Number of threads in a warp : " << prop.warpSize << endl
          << "        Max threads in one block    : " << prop.maxThreadsPerBlock << endl
